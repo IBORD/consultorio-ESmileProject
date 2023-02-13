e46fed43751721392a536140f0a9d69b52da2ae7
Guilherme Rutemberg <guilhermerutemfelix@gmail.com>
2023-02-13 04:20:25 -0300

    first commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+class MyComponent extends React.Component<> {}
+class MyComponent extends React.Component<> {}
+class MyComponent extends React.Component<> {
+    state = {};
+}
+class MyComponent extends React.Component<> {
+    state = getState();
+}
+class MyComponent extends React.Component<> {
+}
